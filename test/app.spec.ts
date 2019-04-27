fbbe8c48e6bd3094ab7fe2ad699b9f9ea634b815
andrewc2020 <andrewc2020@gmail.com>
2019-04-27 17:30:02 +0100

    added integration test

---
+++ test/app.spec.ts
@@ -0,0 +1,13 @@
+import request from 'supertest';
+import app from '../src/app';
+
+describe('Get', () => {
+it('fetches Hello World!', (done) => {
+    return request(app)
+    .get('/')
+    .end((err, res: any) => {
+        expect(res.text).toBe('Hello World!');
+        done();
+    });
+});
+});
